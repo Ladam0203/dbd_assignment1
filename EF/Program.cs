06cbd848de6987925442865c25c1aa8a0deca2ad
Ádám Lőrincz <90683098+Ladam0203@users.noreply.github.com>
2024-03-04 11:06:06 +0100

    Deleted old EF project

--- EF/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Runtime.CompilerServices;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
-
-EshopContext context = new EshopContext(); 
-
-context.Database.EnsureCreated();
