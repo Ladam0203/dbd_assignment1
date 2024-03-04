1278074ee1ce2f036e9f2f857eb1f4b1d1874a1a
Ádám Lőrincz <90683098+Ladam0203@users.noreply.github.com>
2024-03-04 10:23:01 +0100

    Category to Products: migration via EF

---
+++ EFMigrations/Migrations/20240304092046_AddCategoryToProduct.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "ProductTable",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductTable_CategoryId",
+                table: "ProductTable",
+                column: "CategoryId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductTable_Category_CategoryId",
+                table: "ProductTable",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductTable_Category_CategoryId",
+                table: "ProductTable");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductTable_CategoryId",
+                table: "ProductTable");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "ProductTable");
+        }
+    }
+}
