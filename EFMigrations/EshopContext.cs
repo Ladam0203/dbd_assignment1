4fbd4e10116f9bcaa95b5fcb0b1e06ed73b17f8c
Tawfik <90477695+TawfikAzza@users.noreply.github.com>
2024-03-04 10:16:52 +0100

    Ef/initial setup Migration Initial Create + update (#11)
    * Initial EF Setup with Product table
    
    * Error Handling push
    
    * Created .gitignore
    
    * Changed the project name
    
    ---------
    
    Co-authored-by: Ádám Lőrincz <90683098+Ladam0203@users.noreply.github.com>

---
+++ EFMigrations/EshopContext.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFMigrations;
+
+public class EshopContext : DbContext
+{
+
+    public DbSet<Product> ProductTable { get; set; }
+
+    // The following configures EFMigrations to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source=./db.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) 
+    {
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Id)
+            .ValueGeneratedOnAdd();
+    }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ EFMigrations/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+using EFMigrations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+EshopContext context = new EshopContext(); 
+
+context.Database.EnsureCreated();
